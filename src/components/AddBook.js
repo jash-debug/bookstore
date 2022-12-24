94dccd4195aa250f49d34aba4444dac95f6859fc
jash-debug <jash-debug@github.com>
2022-12-24 17:08:31 +0300

    implement suggested changes

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -4,8 +4,8 @@ const AddBook = () => (
   <section className="add-book-form">
     <h2>ADD NEW BOOK</h2>
     <form className="books-list">
+      <input type="text" placeholder="Author" />
       <input type="text" placeholder="Book title" />
-
       <button type="button">ADD BOOK</button>
     </form>
   </section>
