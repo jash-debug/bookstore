62a3bf375b0fc47c8f4709b525bca68a2dbe022d
jash-debug <jash-debug@github.com>
2022-12-24 15:10:38 +0300

    add react template files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import React from "react";
+
+class App extends React.Component {
+	constructor(props) {
+	  super(props);
+	  this.state = {};
+	}
+  
+	render() {
+	  return (
+		null
+	  );
+	}
+  }
+  
+  export default App;
