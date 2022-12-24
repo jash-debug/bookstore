84cc8d3cbdf00ccfbf8daf43f21ca33c245de4a1
jash-debug <jash-debug@github.com>
2022-12-24 16:15:21 +0300

    create routes for book page and categories

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import "./App.css";
-import React from "react";
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import "./App.css"
+import Categories from './components/Categories';
+import BooksPage from './components/BooksPage';
 
-class App extends React.Component {
-	constructor(props) {
-	  super(props);
-	  this.state = {};
-	}
-  
-	render() {
-	  return (
-		null
-	  );
-	}
-  }
-  
-  export default App;
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<BooksPage />} />
+          <Route path="/Categories" element={<Categories />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
